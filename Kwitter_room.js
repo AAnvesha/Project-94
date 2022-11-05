9f6cdd5a0ced87d5d750110326750b09ec6e3a51
Anvesha Misra <98341927+AAnvesha@users.noreply.github.com>
2022-11-05 12:43:45 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbkJXrXTTykgiTXmS_A-BzZa7s2jlgrTQ",
+    authDomain: "project-94-975c5.firebaseapp.com",
+    databaseURL: "https://project-94-975c5-default-rtdb.firebaseio.com",
+    projectId: "project-94-975c5",
+    storageBucket: "project-94-975c5.appspot.com",
+    messagingSenderId: "1066634128348",
+    appId: "1:1066634128348:web:2c21816e87e2ec83b77d19",
+    measurementId: "G-B99PSDG7LZ"
+  };
+
+  firebase.initializeApp(firebaseConfig);
